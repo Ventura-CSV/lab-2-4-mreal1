deb663765aebdcde594a8b63245391ee68482242
Marizza Real <realmarizza@gmail.com>
2023-09-02 17:20:34 -0700

    lab4

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     # Comlete your code here
     ##################################################
     original_str = "Python Programing"
-    sub1 = original_str[:6]
+    sub1 = original_str[:6:]
     sub2 = original_str[7::]
     merged_str = sub2 + sub1
 
