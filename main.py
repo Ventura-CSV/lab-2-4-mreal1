b0b430fec4ef2f002b096935924380d5ea73479d
Marizza Real <realmarizza@gmail.com>
2023-09-02 17:10:03 -0700

    done

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_str = "Python Programing"
+    sub1 = original_str[:6]
+    sub2 = original_str[7::]
+    merged_str = sub2 + sub1
+
+    print(sub2)
+    print(sub1)
+    print(merged_str)
+
+
     pass
-
-
 if __name__ == '__main__':
     main()
