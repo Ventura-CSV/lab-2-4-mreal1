4aa751a10d3679757d9ad64aec509c2d5281a0a9
Marizza Real <realmarizza@gmail.com>
2023-09-02 17:31:47 -0700

    lab 4

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     original_str = "Python Programing"
     sub1 = original_str[:6:]
     sub2 = original_str[7::]
-    merged_str = sub2 + sub1
+    merged_str = sub2 + '' + sub1
 
     print(sub2)
     print(sub1)
